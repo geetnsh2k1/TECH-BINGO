60d901ac9b2f689a0a4970dd6699066b065ac70e
Geetansh <geetansh2k1@gmail.com>
2021-08-21 22:28:45 +0530

    heroku deploy

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'Static')
@@ -135,3 +136,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
